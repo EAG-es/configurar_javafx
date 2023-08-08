da6778230b95f856422d425a52ad7f529fab35af
emilio <eag2001@gmail.com>
2023-08-08 12:54:23 +0100

    20230808

--- src/main/java/inclui/configurar_javafx/Configurar_javafx.java
+++ src/main/java/inclui/configurar_javafx/Configurar_javafx.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package inclui.configurar_javafx;
 
 import innui.modelos.configuraciones.ResourceBundles;
